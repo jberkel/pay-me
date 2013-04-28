12453315ba49fd571e55fc1bcf267a874f37cc02
Jan Berkel <jan.berkel@gmail.com>
2013-04-28 14:56:46 +0200

    More purchase tests

--- tests/src/test/java/com/github/jberkel/payme/PurchaseTest.java
+++ tests/src/test/java/com/github/jberkel/payme/PurchaseTest.java
@@ -14,7 +14,14 @@ public class PurchaseTest {
     @Test
     public void shouldParsePurchase() throws Exception {
         String purchase = resourceAsString("purchase.json");
-        Purchase p = new Purchase(purchase, "");
+        Purchase p = new Purchase(purchase, "signature");
+
         assertThat(p.getOrderId()).isEqualTo("someOrderId");
+        assertThat(p.getPackageName()).isEqualTo("com.example.test");
+        assertThat(p.getPurchaseTime()).isEqualTo(12345L);
+        assertThat(p.getPurchaseState()).isEqualTo(1);
+        assertThat(p.getDeveloperPayload()).isEqualTo("custom");
+        assertThat(p.getSignature()).isEqualTo("signature");
+        assertThat(p.getOriginalJson()).isEqualTo(purchase);
     }
 }
