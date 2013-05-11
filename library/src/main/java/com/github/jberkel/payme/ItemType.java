b046710fe9f7de8ff37acf84ab4b191e657741b7
Jan Berkel <jan.berkel@gmail.com>
2013-05-11 11:43:23 +0200

    Fix lint warning

--- library/src/main/java/com/github/jberkel/payme/ItemType.java
+++ library/src/main/java/com/github/jberkel/payme/ItemType.java
@@ -1,11 +1,13 @@
 package com.github.jberkel.payme;
 
+import java.util.Locale;
+
 public enum ItemType {
     INAPP,
     SUBS,
     UNKNOWN;
 
     public String toString() {
-        return name().toLowerCase();
+        return name().toLowerCase(Locale.ENGLISH);
     }
 }
