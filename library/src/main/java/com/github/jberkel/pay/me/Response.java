4f8fc652a0073e5b5d720c0362c150806ff0740e
Jan Berkel <jan.berkel@gmail.com>
2013-05-25 10:23:19 +0200

    Formatting

--- library/src/main/java/com/github/jberkel/pay/me/Response.java
+++ library/src/main/java/com/github/jberkel/pay/me/Response.java
@@ -1,6 +1,7 @@
 package com.github.jberkel.pay.me;
 
 public enum Response {
+    // response codes from the IAB service
     OK                 (0, R.string.pay_me_response_ok, "OK"),
     USER_CANCELED      (1, R.string.pay_me_response_user_canceled, "User Canceled"),
     BILLING_UNAVAILABLE(3, R.string.pay_me_response_billing_unavailable, "Billing Unavailable"),
@@ -10,15 +11,16 @@ public enum Response {
     ITEM_ALREADY_OWNED (7, R.string.pay_me_response_item_already_owned, "Item Already Owned"),
     ITEM_NOT_OWNED     (8, R.string.pay_me_response_item_not_owned, "Item not owned"),
 
-    IABHELPER_REMOTE_EXCEPTION(-1001,R.string.pay_me_response_remote_exception, "Remote exception during initialization"),
-    IABHELPER_BAD_RESPONSE(-1002, R.string.pay_me_response_bad_response, "Bad response received"),
-    IABHELPER_VERIFICATION_FAILED(-1003, R.string.pay_me_response_signature_verification_failed, "Purchase signature verification failed"),
-    IABHELPER_SEND_INTENT_FAILED(-1004, R.string.pay_me_response_send_intent_failed, "Send intent failed"),
-    IABHELPER_UNKNOWN_PURCHASE_RESPONSE(-1006, R.string.pay_me_response_unknown_purchase_response, "Unknown purchase response"),
-    IABHELPER_MISSING_TOKEN(-1007, R.string.pay_me_response_missing_token, "Missing token"),
-    IABHELPER_UNKNOWN_ERROR(-1008, R.string.pay_me_response_unknown_error, "Unknown error"),
+    // internal response codes
+    IABHELPER_REMOTE_EXCEPTION           (-1001, R.string.pay_me_response_remote_exception, "Remote exception during initialization"),
+    IABHELPER_BAD_RESPONSE               (-1002, R.string.pay_me_response_bad_response, "Bad response received"),
+    IABHELPER_VERIFICATION_FAILED        (-1003, R.string.pay_me_response_signature_verification_failed, "Purchase signature verification failed"),
+    IABHELPER_SEND_INTENT_FAILED         (-1004, R.string.pay_me_response_send_intent_failed, "Send intent failed"),
+    IABHELPER_UNKNOWN_PURCHASE_RESPONSE  (-1006, R.string.pay_me_response_unknown_purchase_response, "Unknown purchase response"),
+    IABHELPER_MISSING_TOKEN              (-1007, R.string.pay_me_response_missing_token, "Missing token"),
+    IABHELPER_UNKNOWN_ERROR              (-1008, R.string.pay_me_response_unknown_error, "Unknown error"),
     IABHELPER_SUBSCRIPTIONS_NOT_AVAILABLE(-1009, R.string.pay_me_response_subscriptions_not_available,  "Subscriptions not available"),
-    IABHELPER_INVALID_CONSUMPTION(-1010, R.string.pay_me_response_invalid_consumption, "Invalid consumption attempt");
+    IABHELPER_INVALID_CONSUMPTION        (-1010, R.string.pay_me_response_invalid_consumption, "Invalid consumption attempt");
 
     /** the error code */
     public final int code;
